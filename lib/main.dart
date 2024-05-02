8b6d58c9bcadd3c798a12ffcc95a23b222b3fc2a
Michael Gunawan <michaelgunawan_2226250062@mhs.mdp.ac.id>
2024-05-02 17:38:36 +0700

    lanjut

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,10 @@ import 'package:flutterlist/userlist.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(UserList());
 }
