0bc75fd5234723adfa593320148783445afa1274
Michael Gunawan <michaelgunawan_2226250062@mhs.mdp.ac.id>
2024-05-02 17:35:11 +0700

    Screenshot

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutterlist/useritem.dart';
 import 'package:flutterlist/userlist.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
   runApp(UserList());
